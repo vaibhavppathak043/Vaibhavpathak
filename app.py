c8ac602c3997066171405b8926a82bc61bb4cafd
vaibhavppathak043 <vaibhavppathak043@gmail.com>
2026-07-10 11:34:06 +0530

    Refactor import statement for LogisticRegression

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ Generic form of same equation is,
 import pandas as pd
 import streamlit as st
 import numpy as np
-from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('/content/sample_data/homeprices.csv')
